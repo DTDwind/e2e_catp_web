fb7999f2154f3a9e6477b57cb92a1551325ed7fc
DTDwind <sam841009@yahoo.com.tw>
2019-12-20 13:40:57 +0800

    fix some detail

---
+++ run_decode.sh
@@ -0,0 +1,7 @@
+wav_name="QAQ"
+
+. utils/parse_options.sh || exit 1;
+# echo {{$wav_name}};
+# 20191219164236.wav
+echo "U0003_DS0001 /share/nas165/chengsam/www/MD_v5_chengsam/wav/$wav_name" > /share/nas165/chengsam/espnet20191017/espnet/egs/E2E_CAPT_web_20191205/L1_T/data/CAPT_WEB_test/wav.scp;
+/share/nas165/chengsam/espnet20191017/espnet/egs/E2E_CAPT_web_20191205/L1_T/run_CAPT_web_Decoder.sh
